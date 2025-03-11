9bb44c19ce3dd9a021ecdb77850da63153cd879a
Andrii Mishkovskyi <548482+mishok13@users.noreply.github.com>
2025-03-11 21:20:13 +0100

    Brew setup on linux

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,4 +1,15 @@
 if status is-interactive
+    set brewcmd (path filter /opt/homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin/brew)[1]
+    and $brewcmd shellenv | source
+
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+
     fzf_configure_bindings --directory=\ec --history=\e\cr
     atuin init fish | source
 
