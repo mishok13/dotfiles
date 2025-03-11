ea0bd285d3caa61a48cf60043c49e60d26614731
Andrii Mishkovskyi <548482+mishok13@users.noreply.github.com>
2025-03-11 21:23:20 +0100

    fishies (#77)
    * Fish support
    
    * Fixing it?
    
    * Thar she blows
    
    * liek dis
    
    * okie dokie
    
    * ffs
    
    * Brew setup on linux

---
+++ config/fish/.config/fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    set brewcmd (path filter /opt/homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin/brew)[1]
+    and $brewcmd shellenv | source
+
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+
+    fzf_configure_bindings --directory=\ec --history=\e\cr
+    atuin init fish | source
+
+    abbr -a -g k kubectl
+    abbr -a -g kex kubectl exec -i -t
+    abbr -a -g kg kubectl get
+    abbr -a -g kgp kubectl get pods
+    abbr -a -g kx kubectx
+    abbr -a -g kns kubens
+
+    abbr -a -g l eza
+    abbr -a -g la eza -la
+    abbr -a -g lt eza -T
+
+end
