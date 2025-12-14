11d81c43e9920b7e10eff5d6b9596ad2a4b5ee63
Andrii Mishkovskyi <548482+mishok13@users.noreply.github.com>
2025-12-14 19:39:58 +0100

    Add GPU drivers

--- nixos/beafiboi.nix
+++ nixos/beafiboi.nix
@@ -12,4 +12,10 @@
   ];
   networking.hostName = "beafiboi";
   system.stateVersion = "25.05";
+
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false;
+
+  nixpkgs.config.allowUnfree = true;
 }
