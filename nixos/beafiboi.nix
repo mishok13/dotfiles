64cfc090e54b5f8bc8a81baf694e223e93e0122c
Andrii Mishkovskyi <548482+mishok13@users.noreply.github.com>
2026-01-14 14:34:37 +0100

    Add Ollama to beafiboi (#228)
    * Add Ollama to beafiboi
    
    * Remove unused docker compose config for Ollama and WebUI

--- nixos/beafiboi.nix
+++ nixos/beafiboi.nix
@@ -14,7 +14,6 @@
   system.stateVersion = "25.05";
 
   hardware.graphics.enable = true;
-
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
@@ -24,4 +23,28 @@
   hardware.nvidia-container-toolkit.enable = true;
 
   nixpkgs.config.allowUnfree = true;
+
+  services.ollama = {
+    enable = true;
+    package = pkgs.ollama-cuda.override {
+      # Workaround from https://github.com/NixOS/nixpkgs/issues/421775#issuecomment-3689793418
+      # Query with `nvidia-smi --query-gpu=compute_cap`
+      cudaArches = [ "61" ];
+    };
+    loadModels = [
+      "qwen2.5-coder:7b"
+      "codellama:7b"
+      "deepseek-coder-v2:7b"
+
+      "llama3.2:7b"
+      "mistral:7b"
+      "qwen2.5:7b"
+      "llama3.1:8b"
+
+      "llama3.2:3b"
+      "phi3:3.8b"
+    ];
+    openFirewall = true;
+    host = "0.0.0.0";
+  };
 }
